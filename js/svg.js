dc643e0b3f70db829e85bc2c80ec85e766007179
William Peirce <bill@seeyourcampsite.com>
2019-05-04 20:48:56 -0400

    added about section

--- js/svg.js
+++ js/svg.js
@@ -1,20 +1,23 @@
 $(function() {
 
+//**********************************************************************/
+//*** TECH *************************************************************/
+
+  // Close tech pop-up on click away
   $(document).mouseup(e => {
     const $techGrid = $(".tech--grid");
     const $techPopup = $(".tech--popup");
 
-    // if the target of the click isn't the tech grid nor a descendant of the tech grid
+    // if the target of the click is neither the tech grid nor a descendant.
     if (!$techGrid.is(e.target) && $techGrid.has(e.target).length === 0) {
       $techPopup.slideUp(500);
       $(".tech--grid-icon").removeClass("active");
     }
   });
 
-  // TECH POP-UP
+  // Add event listeners to tech icons.
   const technologies = $(".tech--grid-icon");
 
-  // Add event listeners to tech icons.
   technologies.each((i, technology) => {
     const $tech = $(technology);
     $tech.on("click", function() {
@@ -33,14 +36,16 @@ $(function() {
     });
   });
 
-  // Helper function to display tech pop-up.
-  function displayTechPopup(gridIcon, origPercent, techName, techText) {
+//*** TECH HELPER FUNCTIONS ********************************************/
+
+  function displayTechPopup(gridIcon, newPercent, techName, techText) {
 
     const $techPopup = $(".tech--popup");
     const $techExpName = $($techPopup.find("#tech--popup-experience-name")[0]);
     const $techExpDesc = $($techPopup.find("#tech--popup-experience-description")[0]);
     const $techProfText = $($techPopup.find(".tech--popup-proficiency-text")[0]);
     const $percentSpan = $($techProfText.find("h1 span")[0]);
+    let oldPercent = parseInt($percentSpan.text());
     const circleGroups = $($techPopup.find("svg g"));
 
     // Set new tech name and description
@@ -50,68 +55,68 @@ $(function() {
     const isFirstTime = $techPopup.css('display') === "none";
 
     if (isFirstTime) {
-      $techPopup.slideDown(1000);
+      $techPopup.slideDown(500);
+      // This loop hides the previous proficiency graphic before sliding open.
       for (let i = 0; i <= circleGroups.length - 1; i++) {
         $techProfText.addClass("vis-hidden");
-        let jCircleGroup = $(circleGroups[i]);
-        jCircleGroup.find("path").each((index, path) => {
-          const jPath = $(path);
-          jPath.removeClass("tech--popup-proficiency-fill");
-          jPath.addClass("display-none");
-        });
-      }
-    } else {
-      // FIRST LOOP: hides currently displayed circles
-      for (let i = 0; i <= circleGroups.length - 1; i++) {
-        let jCircleGroup = $(circleGroups[i]);
-        setTimeout(() => {
-          let num = Math.floor((Math.random() * origPercent)).toString();
-          if (num.length === 1) num = "0" + num
-          $percentSpan.text(num);
-          jCircleGroup.find("path").each((index, path) => {
-            const jPath = $(path);
-            jPath.removeClass("display-none");
-          });
-        }, 15 * i);
+        toggleHideCircles(circleGroups, i);
       }
     }
 
-    // Loop through all 36 sets of circles
+    // Removes old gold color (and runs the randomizer and 
+    // hides all circles if the pop-up was already open.).
     for (let i = 0; i <= circleGroups.length - 1; i++) {
-      let jCircleGroup = $(circleGroups[i]);
       setTimeout(() => {
-        jCircleGroup.find("path").each((index, path) => {
-          const jPath = $(path);
-          jPath.removeClass("tech--popup-proficiency-fill");
-          jPath.addClass("display-none");
-        });
+        toggleCircleColors(circleGroups, oldPercent, i);
+        if (!isFirstTime) {
+          toggleHideCircles(circleGroups, i);
+          percentRandomizer($percentSpan, newPercent);
+        }
       }, 15 * i);
-      setTimeout(() => {
-        jCircleGroup.find("path").each((index, path) => {
-          const jPath = $(path);
-          jPath.removeClass("display-none");
-        });
-      }, 15 * (i + 36) + (isFirstTime ? 300 : 0));
-    }
 
-    // Then loop through XX% of circles and apply color fill.
-    for (let i = 0; i <= Math.floor(36 * (origPercent / 100)); i++) {
+      // Shows circles.
+      setTimeout(() => {
+        toggleHideCircles(circleGroups, i);
+      }, 15 * (i + 36));
+
+      // Applies new gold color.
+      // If pop-up was already open, this will run
+      // simultaneously with the above setTimeout.
       setTimeout(() => {
         $techProfText.removeClass("vis-hidden");
-        if (i === Math.floor(36 * (origPercent / 100))) {
-          $percentSpan.text(origPercent);
+        if (i >= Math.floor(36 * (newPercent / 100))) {
+          if (newPercent.length === 1) newPercent = "0" + newPercent;
+          $percentSpan.text(newPercent);
         } else {
-          let num = Math.floor((Math.random() * origPercent)).toString();
-          if (num.length === 1) num = "0" + num
-          $percentSpan.text(num);
+          percentRandomizer($percentSpan, newPercent);
+          toggleCircleColors(circleGroups, newPercent, i, false);
         }
-        let jCircleGroup = $(circleGroups[i]);
-        jCircleGroup.find("path").each((index, path) => {
-          const jPath = $(path);
-          jPath.addClass("tech--popup-proficiency-fill");
-        });
-      }, 15 * (i + (isFirstTime ? 72 : 36)) + (isFirstTime ? 300 : 0));
+      }, 15 * (i + (isFirstTime ? 72 : 36)));
     }
   };
 
 });
+
+function percentRandomizer($percentSpan, percent) {
+  let num = Math.floor((Math.random() * percent)).toString();
+  if (num.length === 1) num = "0" + num;
+  $percentSpan.text(num);
+}
+
+function toggleCircleColors(circleGroups, percent, i, isOldPercent) {
+  let $jCircleGroup = $(circleGroups[i]);
+  $jCircleGroup.find("path").each((index, path) => {
+    const $path = $(path);
+    if (i < Math.floor(36 * (percent / 100))) {
+      $path.toggleClass("tech--popup-proficiency-fill");
+    }
+  });
+}
+
+function toggleHideCircles(circleGroups, i) {
+  let $jCircleGroup = $(circleGroups[i]);
+  $jCircleGroup.find("path").each((index, path) => {
+    const $path = $(path);
+    $path.toggleClass("display-none");
+  });
+}
