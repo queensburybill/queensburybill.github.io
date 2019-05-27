7a5234e898d3b1d8980c2d9bbbb007dc0b4a03ba
William Peirce <bill@seeyourcampsite.com>
2019-05-27 12:20:31 -0400

    added responsive header

--- js/main.js
+++ js/main.js
@@ -30,7 +30,35 @@ $(function() {
       $img.replaceWith($svg);
     }, "xml");
   });
-  
+
+  $("#menu-btn").on("click", function() {
+    const $navList = $("#nav-list");
+    $navList.slideToggle(500);
+    $(this).toggleClass("active");
+    $("#nav-navbar").toggleClass("active");
+    $("#nav-list").toggleClass("active");
+  });
+
+  $("#nav-list li").each((i, li) => {
+    const $navList = $("#nav-list");
+    $(li).on("click", () => {
+      $navList.slideToggle(500);
+      $("#menu-btn").toggleClass("active");
+      $("#nav-navbar").toggleClass("active");
+      $("#nav-list").toggleClass("active");
+    });
+  });
+
+});
+
+$(window).on("resize", function() {
+  const $navList = $("#nav-list");
+  if ($navList.width() >= $(window).width()) {
+    $("#menu-btn").removeClass("active");
+    $("#nav-navbar").removeClass("active");
+    $("#nav-list").removeClass("active");
+    $navList.slideUp(500);
+  }
 });
 
 
